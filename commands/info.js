3f54e3153bde280fd322896c740c52423904c603
HyRo <generalkubo@gmail.com>
2021-10-09 21:52:05 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -3,6 +3,7 @@ module.exports = (client, msg, args) => {
         `__**Info**__`,
         ``,
         `Beta Hiven (self)bot.`,
+        `Github: https://github.com/Garlic-Team/hivenbot`,
         `SFE: https://hiven.house/CvdBW9`
     ].join('\n'))
 }
