b69cbe0e9d9e2f520659ee69c01d04be247176a0
HyRo <generalkubo@gmail.com>
2021-10-09 21:46:35 +0200

    Add files via upload

---
+++ commands/info.js
@@ -0,0 +1,8 @@
+module.exports = (client, msg, args) => {
+    msg.room.send([
+        `__**Info**__`,
+        ``,
+        `Beta Hiven (self)bot.`,
+        `SFE: https://hiven.house/CvdBW9`
+    ].join('\n'))
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const { Client } = require('hiven');
+const client = new Client({ type: 'user' });
+const fs = require('fs');
+const prefix = '!';
+const cooldown = new Map();
+
+client.on('init', () => {
+    console.log("[BOT] Bot is ready!")
+});
+
+client.on('message', (msg) => {
+    if(!msg.content.startsWith(prefix)) return;
+
+    const cldwn = cooldown.get(msg.author.id);
+
+    if (Date.now() < cldwn) return;
+    cooldown.set(msg.author.id, Date.now() + 10000);
+
+    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+  
+    if (!(command.match(/^[0-9a-zA-Z]+$/))) return;
+  
+    const file = `${__dirname}/commands/${command}.js`;
+  
+    if (fs.existsSync(file)) {
+      try {
+        (require(file))(client, msg, args);
+      } catch(err) {
+        console.log(err);
+      }
+    }
+});
+
+client.connect(process.env.token)
